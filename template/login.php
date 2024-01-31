b8a7b71ba3f28c6a8f6be4f05d012a393e797512
asadnazifi <143527308+pro-3213@users.noreply.github.com>
2024-01-31 18:51:44 +0330

    register-login

--- template/login.php
+++ template/login.php
@@ -33,15 +33,15 @@
     <div class="card-body login-card-body">
       <p class="login-box-msg">فرم زیر را تکمیل کنید و ورود بزنید</p>
 
-      <form action="../../index2.html" method="post">
+      <form action="register-login.php" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="ایمیل">
+          <input type="email" class="form-control" placeholder="ایمیل" name ="username">
           <div class="input-group-append">
             <span class="fa fa-envelope input-group-text"></span>
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="رمز عبور">
+          <input type="password" class="form-control" placeholder="رمز عبور" name = "password">
           <div class="input-group-append">
             <span class="fa fa-lock input-group-text"></span>
           </div>
---
+++ template/register-login.php
@@ -0,0 +1,34 @@
+<?php
+// اتصال به پایگاه داده
+
+    include_once "../functions/contect-to-db.php";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    $sql = "SELECT * FROM users WHERE gmail = '$username' and password = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        // کاربر اعتبارسنجی شده است
+        $row = mysqli_fetch_assoc($result);
+        if ($row['role'] === 'admin') {
+            // اگر کاربر ادمین است، ورود انجام می‌شود
+            session_start();
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+            header("Location: admin");
+        } else {
+            // اگر کاربر معمولی است، ورود انجام می‌شود
+            session_start();
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+            header("Location: user_panel.php");
+        }
+    } else {
+        // اگر اطلاعات وارد شده اشتباه است
+        echo "نام‌کاربری یا رمزعبور اشتباه است.";
+    }
+    mysqli_close($conn);
+}
+
+?>
